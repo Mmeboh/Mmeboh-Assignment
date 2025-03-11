0630530546c232d323e12f93e835350eb1de2e2c
Mmeboh <mmebohmatthew@gmail.compwdpwdgit config --global user.email mmebohmatthew@gmail.com>
2025-03-11 13:24:43 +0100

    catfish hub

--- obj/Debug/net9.0/Mmeboh222.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mmeboh222.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mmeboh222")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdd56a2c160d1944dcea660cb94afc498b0c970")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mmeboh222")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mmeboh222")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
